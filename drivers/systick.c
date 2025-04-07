669d5712e550d708429feed39232a577df0be4e2
Douglas Summerville <dsummer@binghamton.edu>
2025-04-07 15:29:17 -0400

    Fixed Systick Priority
    Wrong priority field for systick

--- drivers/systick.c
+++ drivers/systick.c
@@ -10,8 +10,8 @@ void configure_systick(uint32_t systick_period_us)
 	ticks.proc0_cycles = 1;
 	m33.syst_rvr= systick_period_us;
 	m33.syst_cvr=0;
-	m33.shpr3 = (m33.shpr3 & ~M33_SHPR3_PRI_14_3_MASK ) 
-		| M33_SHPR3_PRI_14_3(0);
+	m33.shpr3 = (m33.shpr3 & ~M33_SHPR3_PRI_15_3_MASK ) 
+		| M33_SHPR3_PRI_15_3(0);
 	m33.syst_csr= M33_SYST_CSR_CLKSOURCE(0) 
 		| M33_SYST_CSR_ENABLE_MASK 
 		| M33_SYST_CSR_TICKINT(0);
